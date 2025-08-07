afa80dc4e3c4496208121ad5da10f8510aa6d5c8
nachmayuri <131973180+Deos2023@users.noreply.github.com>
2025-08-07 18:45:31 +0530

    update

--- app/page.js
+++ app/page.js
@@ -281,7 +281,7 @@ export default function Home() {
                     <div>
                       <h4 className="font-semibold text-blue-700">Phone</h4>
                       <p className="text-gray-700">7003088484</p>
-                      <p className="text-gray-700">7278749092</p>
+                      <p className="text-gray-700">7278749098</p>
                     </div>
                   </div>
                   <div className="flex items-start">
@@ -435,7 +435,7 @@ export default function Home() {
                   <svg className="w-5 h-5 mr-2 text-teal-300" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"></path>
                   </svg>
-                  <span>7003088484 / 7278749092</span>
+                  <span>7003088484 / 7278749098</span>
                 </li>
                 <li className="flex items-center">
                   <svg className="w-5 h-5 mr-2 text-teal-300" fill="none" stroke="currentColor" viewBox="0 0 24 24">
